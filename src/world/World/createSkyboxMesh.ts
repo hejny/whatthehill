740f62923f6f9de5792510cde07a14b7c7297c3c
hejny <me@pavolhejny.com>
2017-11-25 06:13:38 +0100

    + Design

--- src/world/World/createSkyboxMesh.ts
+++ src/world/World/createSkyboxMesh.ts
@@ -3,7 +3,7 @@ import * as BABYLON from 'babylonjs';
 export default function createSkyboxMesh(scene:BABYLON.Scene):BABYLON.AbstractMesh{
 
     scene.fogMode = BABYLON.Scene.FOGMODE_EXP;
-    scene.fogColor = BABYLON.Color3.FromHexString('#395aff');
+    scene.fogColor = BABYLON.Color3.FromHexString('#c8e7ff');
     scene.fogDensity = 0.01;
 
 
