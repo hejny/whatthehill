1643236113f88f9a329a1bac05be4a341588c684
hejny <me@pavolhejny.com>
2017-11-25 02:30:24 +0100

    + GeoLabel

--- src/generator/index.ts
+++ src/generator/index.ts
@@ -20,39 +20,51 @@ export default class WorldGenerator {
         groundMaterial.diffuseColor = BABYLON.Color3.FromHexString('#ffffff');
         //groundMaterial.alpha = 0.7;
         groundMaterial.alpha = 1;
+        groundMaterial.freeze();
 
 
         for (const tile of data.tiles) {
-
-
             console.log(tile);
-            const groundMesh = BABYLON.Mesh.CreateGroundFromHeightMap("groundTile", tile.url,
-                200, 200,
-                100,//subdivs
-                -30, 10,
-                this.world.scene, false);
+
+            var options = {
+                width: 200,
+                height: 200,
+                subdivisions: 200,
+                minHeight: -30,
+                maxHeight: 10,
+                onReady: (groundMesh: BABYLON.GroundMesh) => {
+
+                    //console.log(groundMesh);
+                    //console.log(groundMesh.getHeightAtCoordinates(0,0));
+
+                    for (let i = 0; i < 100; i++) {
+
+                        const
+                            x = (Math.random() - .5) * 200,
+                            z = (Math.random() - .5) * 200;
+
+
+                        new GeoLabel(
+                            this.world,
+                            new BABYLON.Vector3(
+                                x,
+                                groundMesh.getHeightAtCoordinates(x, z),
+                                z
+                            ));
+                    }
+
+
+                }
+            };
+            const groundMesh = BABYLON.MeshBuilder.CreateGroundFromHeightMap("ground", tile.url, options, this.world.scene);
+            /*const groundMesh = BABYLON.Mesh.CreateGroundFromHeightMap("groundTile", tile.url,
+             200, 200,
+             512,//subdivs
+             -30, 10,
+             this.world.scene, false);*/
             groundMesh.material = groundMaterial;
 
-
-            //console.log(groundMesh);
-            //console.log(groundMesh.getHeightAtCoordinates(0,0));
-
-
         }
-
-
-
-        for(let i=0;i<100;i++){
-
-            new GeoLabel(
-                this.world,
-                new BABYLON.Vector3(
-                    (Math.random()-.5)*200,
-                    10,
-                    (Math.random()-.5)*200
-                ));
-        }
-
 
 
     }
